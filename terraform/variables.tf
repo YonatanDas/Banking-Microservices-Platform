4c84c82e41cff937660b2f6f880a8e6daf2c4910
YonatanDas <yonatan804@gmail.com>
2025-10-18 18:47:43 +0300

    chore(terraform): add backend configuration and base Terraform setup

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
