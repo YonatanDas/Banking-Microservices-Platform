89236d28662e6d12cd49c7f3f0d87cf91416654e
YonatanDas <yonatan804@gmail.com>
2025-11-08 19:36:38 +0200

    testing

--- .github/workflows/reusable/aws-oidc-login.sh
+++ .github/workflows/reusable/aws-oidc-login.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
-
-ROLE_ARN=$1
-AWS_REGION=$2
-
-echo "🔐 Configuring AWS OIDC credentials..."
-echo "Assuming role: $ROLE_ARN"
-aws-actions/configure-aws-credentials@v2 \
-  --role-to-assume "$ROLE_ARN" \
-  --aws-region "$AWS_REGION" \
-  --audience sts.amazonaws.com
+AWS_REGION=$1
+REGISTRY=$2
 
 echo "Logging in to AWS ECR..." 
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$REGISTRY"
