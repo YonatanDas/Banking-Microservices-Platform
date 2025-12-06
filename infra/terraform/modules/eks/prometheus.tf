ca1a1f29ec40d8a998475b5fa8762e17c0143fcb
YonatanDas <yonatan804@gmail.com>
2025-12-06 14:46:18 +0200

    testing

--- infra/terraform/modules/eks/prometheus.tf
+++ infra/terraform/modules/eks/prometheus.tf
@@ -11,10 +11,9 @@ resource "kubernetes_namespace" "monitoring" {
 }
 
 # Prometheus Operator is now managed by ArgoCD via GitOps
-# Removed helm_release.prometheus_operator to avoid conflicts
 
 output "grafana_admin_password" {
   description = "Grafana admin password"
-  value       = "admin123"
+  value       = "external-secrets"  # Matches the value in dev.yaml
   sensitive   = true
 }
